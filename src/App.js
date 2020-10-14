151b70b71f70d5c94aac261c563fe0d5d91c0d70
Cesar Aleman <cesar.aleman.cal@gmail.com>
2020-10-14 15:46:40 -0700

    Finished with Tut

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
+import wordsToNumbers from 'words-to-numbers';
+
 
 import NewsCards from './components/NewsCards/NewCards';
 import useStyles from './styles.js';
+import { act } from 'react-dom/test-utils';
 // import classes from '*.module.css';
 
 const alanKey = '04a25327e951b7ff9ad30f2835c972bc2e956eca572e1d8b807a3e2338fdd0dc/stage';
@@ -12,13 +15,30 @@ const alanKey = '04a25327e951b7ff9ad30f2835c972bc2e956eca572e1d8b807a3e2338fdd0d
 const App = () => {
     const classes = useStyles();
     const [ newsArticles, setNewsArticles ] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
     
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({command, articles}) => {
+            onCommand: ({command, articles, number}) => {
                 if(command === 'newHeadLines') {
                     setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if(command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                } else if(command === 'open') {
+                    console.log(number);
+                    const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+
+                    if(parsedNumber > 20 ) {
+                        alanBtn().playText('Please try that again.')
+                    } else if(article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...');
+
+                    }
+
                 }
             }
         })
@@ -29,7 +49,7 @@ const App = () => {
             <div className ={classes.logoContainer}>
                 <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
             </div>
-            <NewsCards articles={newsArticles} /> 
+            <NewsCards articles={newsArticles} activeArticle = {activeArticle} /> 
         </div>
     )
 }
